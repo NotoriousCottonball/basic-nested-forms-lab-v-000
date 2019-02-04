c4abf647a573e6dec9247359b00b53dc21d8d726
George  Pianka <gpianka1@gmail.com>
2019-02-04 11:53:13 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe! %i[show edit]) { @user = User.find params[:id] }
+  before_action :set_recipe! only: %i[show edit]) 
   def show
     @recipe = Recipe.find(params[:id])
     if @recipe.ingredients.last.try(:name)
