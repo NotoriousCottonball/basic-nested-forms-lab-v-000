614376f4afd3a37dd8efbe6c832e3f7813d4fae9
George  Pianka <gpianka1@gmail.com>
2019-02-04 11:55:58 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,6 @@ class RecipesController < ApplicationController
   before_action :set_recipe! only: %i{show update}
   
   def show
-    @recipe = Recipe.find(params[:id])
     if @recipe.ingredients.last.try(:name)
       @recipe.ingredients.build
     end
